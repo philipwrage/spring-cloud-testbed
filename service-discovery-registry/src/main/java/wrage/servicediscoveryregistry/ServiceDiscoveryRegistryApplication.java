a1363bc102eddc666ed9dad53c9de9e7d8c347cd
Philip Wrage <philipwrage@users.noreply.github.com>
2021-06-05 07:24:24 -0500

    feature: add module for Eureka service registration & discovery server

---
+++ service-discovery-registry/src/main/java/wrage/servicediscoveryregistry/ServiceDiscoveryRegistryApplication.java
@@ -0,0 +1,13 @@
+package wrage.servicediscoveryregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryRegistryApplication.class, args);
+	}
+
+}
